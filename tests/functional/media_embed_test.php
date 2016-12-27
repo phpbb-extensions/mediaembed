1c2a69adf2d2c78d396ab1ddffaf1ce4c4e8c899
Matt Friedman <maf675@gmail.com>
2016-12-27 11:08:13 -0800

    Update functional tests

--- tests/functional/media_embed_test.php
+++ tests/functional/media_embed_test.php
@@ -15,20 +15,20 @@ namespace phpbb\mediaembed\tests\functional;
  */
 class media_embed_test extends \phpbb_functional_test_case
 {
+	protected $youtubeId = 'PHzShhtkzEk';
+
 	static protected function setup_extensions()
 	{
-		return array('phpbb/mediaembed');
+		return ['phpbb/mediaembed'];
 	}
 
 	public function test_posting_media_bbcode()
 	{
 		$this->login();
 
-		$youtubeId = 'PHzShhtkzEk';
-
-		$post = $this->create_topic(2, 'Media Embed Test Topic 1', "[media]https://youtu.be/{$youtubeId}[/media]");
+		$post = $this->create_topic(2, 'Media Embed Test Topic 1', "[media]https://youtu.be/{$this->youtubeId}[/media]");
 		$crawler = self::request('GET', "viewtopic.php?t={$post['topic_id']}&sid={$this->sid}");
-		static::assertContains("//www.youtube.com/embed/{$youtubeId}", $crawler->filter("#post_content{$post['topic_id']} iframe")->attr('src'));
+		$this->assertContains("//www.youtube.com/embed/{$this->youtubeId}", $crawler->filter("#post_content{$post['topic_id']} iframe")->attr('src'));
 	}
 
 	public function test_media_embed_help()
@@ -39,7 +39,7 @@ class media_embed_test extends \phpbb_functional_test_case
 		$this->assertContainsLang('HELP_EMBEDDING_MEDIA', $crawler->filter('#faqlinks')->text());
 
 		preg_match('/https:\/\/youtu\.be\/(.*)/', $this->lang('HELP_EMBEDDING_MEDIA_DEMO'), $matches);
-		static::assertContains("//www.youtube.com/embed/{$matches[1]}", $crawler->filter('body iframe')->attr('src'));
+		$this->assertContains("//www.youtube.com/embed/{$matches[1]}", $crawler->filter('body iframe')->attr('src'));
 	}
 
 	public function test_acp_modules()
@@ -68,9 +68,9 @@ class media_embed_test extends \phpbb_functional_test_case
 		$query = sprintf('//input[@type="checkbox" and @value="%s"]', $name);
 		$result = $crawler->filterXPath($query);
 
-		$this->assertEquals(
+		$this->assertCount(
 			1,
-			sizeof($result),
+			$result,
 			$message ?: 'Failed asserting that exactly one checkbox with name' .
 				" $name exists in crawler scope."
 		);
