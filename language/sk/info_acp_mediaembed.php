5b74430dfa8a157782f435a79c7377279a5b0c20
Jakub Senko <senky@users.noreply.github.com>
2017-05-16 17:46:24 +0200

    Add Slovak (sk) language

---
+++ language/sk/info_acp_mediaembed.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * phpBB Media Embed PlugIn extension for the phpBB Forum Software package.
+ * Slovak translation by Senky (https://github.com/senky)
+ *
+ * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_PHPBB_MEDIA_EMBED'				=> 'Vkladanie médií',
+	'ACP_PHPBB_MEDIA_EMBED_MANAGE'		=> 'Spravovať stránky',
+	'ACP_PHPBB_MEDIA_EMBED_SETTINGS'	=> 'Nastavenia',
+
+	// Log keys
+	'LOG_PHPBB_MEDIA_EMBED_MANAGE'		=> '<strong>Stránky vkladania médií boli aktualizované</strong>',
+	'LOG_PHPBB_MEDIA_EMBED_SETTINGS'	=> '<strong>Nastavenia vkladania médií boli aktualizované</strong>',
+));
