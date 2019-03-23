eeace12a6b034abfe6dcaa3b3c0818e6e3c28e8d
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2019-03-23 22:58:36 +0300

    Update acp.php

--- language/ar/acp.php
+++ language/ar/acp.php
@@ -6,7 +6,7 @@
  * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
- * Translated By : Bassel Taha Alhitary - www.alhitary.net
+ * Translated By : Bassel Taha Alhitary <http://alhitary.net>
  */
 
 if (!defined('IN_PHPBB'))
@@ -22,13 +22,15 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	// Settings
 	'ACP_MEDIA_SETTINGS'				=> 'إعدادات إدراج مُحتوى الوسائط',
-	'ACP_MEDIA_SETTINGS_EXPLAIN'		=> 'من هنا تستطيع ضبط الإعدادات الخاصة بالإضافة : إدراج مُحتوى الوسائط.',
-	'ACP_MEDIA_DISPLAY_BBCODE'			=> 'تفعيل ',
-	'ACP_MEDIA_DISPLAY_BBCODE_EXPLAIN'	=> 'اختيارك "لا" يعني منع ظهور زر الـ BBCode : <samp>[MEDIA]</samp> في مُحرر الكتابة ولكن يستطيع الأعضاء استخدام الوسم في مُشاركاتهم',
-	'ACP_MEDIA_ALLOW_SIG'				=> 'التوقيعات ',
-	'ACP_MEDIA_ALLOW_SIG_EXPLAIN'		=> 'السماح للعضو بإستخدام هذه الإضافة في التوقيع.',
-	'ACP_MEDIA_SITE_TITLE'				=> 'الموقع : %s',
-	'ACP_MEDIA_SITE_DISABLED'			=> 'هذا الموقع يتعارض مع BBCode آخر موجود : [%s]',
+	'ACP_MEDIA_SETTINGS_EXPLAIN'		=> 'من هنا تستطيع ضبط الإعدادات الخاصة بالإضافة “إدراج مُحتوى الوسائط”.',
+	'ACP_MEDIA_DISPLAY_BBCODE'			=> 'ظهور زر BBCode للوسائط <samp>[MEDIA]</samp>',
+	'ACP_MEDIA_DISPLAY_BBCODE_EXPLAIN'	=> 'اختيارك “لا”, يعني منع ظهور زر BBCode الخاص بالوسائط <samp>[MEDIA]</samp> في محرر الكتابة, وبالرغم من ذلك يستطيع الأعضاء استخدام الوسم <samp>[media]</samp> في مُشاركاتهم.',
+	'ACP_MEDIA_ALLOW_SIG'				=> 'التوقيعات',
+	'ACP_MEDIA_ALLOW_SIG_EXPLAIN'		=> 'السماح للعضو بإستخدام وسم الوسائط في التوقيع.',
+	'ACP_MEDIA_PARSE_URLS'				=> 'تحويل الروابط العادية',
+	'ACP_MEDIA_PARSE_URLS_EXPLAIN'		=> 'اختار “نعم” لتحويل الروابط العادية (التي لم تندرج بين الوسوم <samp>[media]</samp> أو <samp>[url]</samp>) إلى محتوى الوسائط. مع الملاحظة بأن تطبيق هذا الخيار سيكون على المشاركات الجديدة فقط, ولن تؤثر على المشاركات القديمة.',
+	'ACP_MEDIA_SITE_TITLE'				=> 'الموقع: %s',
+	'ACP_MEDIA_SITE_DISABLED'			=> 'هذا الموقع يتعارض مع BBCode آخر موجود: [%s]',
 
 	// Manage sites
 	'ACP_MEDIA_MANAGE'					=> 'إدارة مواقع الوسائط',
