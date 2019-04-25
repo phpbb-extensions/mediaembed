949617197b07a29b227b8f68ff8addaf3ee4bc73
Matt Friedman <maf675@gmail.com>
2019-04-25 13:51:39 -0700

    Add Estonian

---
+++ language/et/info_acp_mediaembed.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * phpBB Media Embed PlugIn extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_PHPBB_MEDIA_EMBED'				=> 'Manustatud meedia',
+	'ACP_PHPBB_MEDIA_EMBED_MANAGE'		=> 'Halda veebilehti',
+	'ACP_PHPBB_MEDIA_EMBED_SETTINGS'	=> 'Seaded',
+
+	// Log keys
+	'LOG_PHPBB_MEDIA_EMBED_MANAGE'		=> '<strong>Uuendati laienduse manustatud meedia veebilehti</strong>',
+	'LOG_PHPBB_MEDIA_EMBED_SETTINGS'	=> '<strong>Manustatud meedia seaded on uuendatud</strong>',
+));
