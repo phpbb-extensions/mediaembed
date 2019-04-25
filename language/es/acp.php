25c00fcde69ccd758ea907c50da18d3f5a7cdf3a
Matt Friedman <maf675@gmail.com>
2019-04-25 13:16:46 -0700

    Update Spanish

--- language/es/acp.php
+++ language/es/acp.php
@@ -26,8 +26,8 @@ $lang = array_merge($lang, array(
 	'ACP_MEDIA_DISPLAY_BBCODE_EXPLAIN'	=> 'Si se deshabilita, el botón BBCode no será mostrado, pero los usuarios aún podrán seguir usando la etiqueta <samp>[media]</samp> en sus mensajes',
 	'ACP_MEDIA_ALLOW_SIG'				=> 'Permitir en firmas de usuario',
 	'ACP_MEDIA_ALLOW_SIG_EXPLAIN'		=> 'Permitir que las firmas de usuario muestren contenido multimedia incorporado.',
-	'ACP_MEDIA_PARSE_URLS'				=> 'Convert plain URLs',
-	'ACP_MEDIA_PARSE_URLS_EXPLAIN'		=> 'Enable this to convert plain URLs (not wrapped in <samp>[media]</samp> or <samp>[url]</samp> tags) to embedded media content. Note that changing this setting will only affect new posts, as existing posts have already been parsed.',
+	'ACP_MEDIA_PARSE_URLS'				=> 'Convertir URLs simples',
+	'ACP_MEDIA_PARSE_URLS_EXPLAIN'		=> 'Habilite esto para convertir URLs simples (no envueltas en etiquetas <samp>[media]</samp> o <samp>[url]</samp>) en contenido multimedia incrustado. Tenga en cuenta que cambiar esta configuración solo afectará a los nuevos mensajes, ya que los mensajes existentes ya se han analizado.',
 	'ACP_MEDIA_SITE_TITLE'				=> 'ID del sitio: %s',
 	'ACP_MEDIA_SITE_DISABLED'			=> 'Este sitio está en conflicto con un BBCode existente: [%s]',
 
@@ -35,5 +35,5 @@ $lang = array_merge($lang, array(
 	'ACP_MEDIA_MANAGE'					=> 'Gestionar sitios de Media Embed',
 	'ACP_MEDIA_MANAGE_EXPLAIN'			=> 'Aquí puede gestionar los sitios que desea permitir en el PlugIn Media Embed, y mostrar su contenido.',
 	'ACP_MEDIA_SITES_ERROR'				=> 'No hay sitios para mostrar.',
-	'ACP_MEDIA_SITES_MISSING'			=> 'The following sites are no longer supported or working. Please re-submit this page to remove them.',
+	'ACP_MEDIA_SITES_MISSING'			=> 'Estos sitios ya no son compatibles o funcionan. Por favor envíe este formulario para eliminarlos.',
 ));
