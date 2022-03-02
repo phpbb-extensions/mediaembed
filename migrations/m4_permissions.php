dbb0a1cdd0b200d2459ddcaae1358a1b3087a430
Matt Friedman <maf675@gmail.com>
2022-03-01 21:05:48 -0800

    Check if default permission roles exists before trying to add them
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- migrations/m4_permissions.php
+++ migrations/m4_permissions.php
@@ -48,15 +48,33 @@ class m4_permissions extends \phpbb\db\migration\migration
 		return [
 			// Add forum permission
 			['permission.add', ['f_mediaembed', false]],
-			['permission.permission_set', ['ROLE_FORUM_FULL', 'f_mediaembed']],
-			['permission.permission_set', ['ROLE_FORUM_POLLS', 'f_mediaembed']],
-			['permission.permission_set', ['ROLE_FORUM_ONQUEUE', 'f_mediaembed']],
-			['permission.permission_set', ['ROLE_FORUM_STANDARD', 'f_mediaembed']],
+			['if', [
+				['permission.role_exists', ['ROLE_FORUM_FULL']],
+				['permission.permission_set', ['ROLE_FORUM_FULL', 'f_mediaembed']],
+			]],
+			['if', [
+				['permission.role_exists', ['ROLE_FORUM_POLLS']],
+				['permission.permission_set', ['ROLE_FORUM_POLLS', 'f_mediaembed']],
+			]],
+			['if', [
+				['permission.role_exists', ['ROLE_FORUM_ONQUEUE']],
+				['permission.permission_set', ['ROLE_FORUM_ONQUEUE', 'f_mediaembed']],
+			]],
+			['if', [
+				['permission.role_exists', ['ROLE_FORUM_STANDARD']],
+				['permission.permission_set', ['ROLE_FORUM_STANDARD', 'f_mediaembed']],
+			]],
 
 			// Add PM permission
 			['permission.add', ['u_pm_mediaembed']],
-			['permission.permission_set', ['ROLE_USER_FULL', 'u_pm_mediaembed']],
-			['permission.permission_set', ['ROLE_USER_STANDARD', 'u_pm_mediaembed']],
+			['if', [
+				['permission.role_exists', ['ROLE_USER_FULL']],
+				['permission.permission_set', ['ROLE_USER_FULL', 'u_pm_mediaembed']],
+			]],
+			['if', [
+				['permission.role_exists', ['ROLE_USER_STANDARD']],
+				['permission.permission_set', ['ROLE_USER_STANDARD', 'u_pm_mediaembed']],
+			]],
 			['permission.permission_set', ['REGISTERED', 'u_pm_mediaembed', 'group']],
 			['permission.permission_set', ['REGISTERED_COPPA', 'u_pm_mediaembed', 'group']],
 		];
