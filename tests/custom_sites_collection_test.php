ded162123c45abaffee0bc0be25fc0eebf23d9ba
Matt Friedman <maf675@gmail.com>
2023-01-07 08:39:25 -0800

    Remove redgifs because its pron
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- tests/custom_sites_collection_test.php
+++ tests/custom_sites_collection_test.php
@@ -51,7 +51,6 @@ class custom_sites_collection_test extends \phpbb_test_case
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/ebaumsworld.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/moddb.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/ok.yml', $collection);
-		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/redgifs.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/schooltube.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/snotr.yml', $collection);
 		$this->assertContains('phpBB/ext/phpbb/mediaembed/collection/sites/videopress.yml', $collection);
