6aedbcddae6fcaadc9c13a027a4389e16910f5e3
Matt Friedman <maf675@gmail.com>
2026-08-12 15:36:17 -0700

    Update Polish and French

--- language/fr/ucp.php
+++ language/fr/ucp.php
@@ -6,6 +6,10 @@
  * @copyright (c) 2025 phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
+ * Language : French [fr]
+ * Translators :
+ * 1. Fred rimbert (https://forums.caforum.fr) (2.0.3) (01.2026)
+ *
  */
 
 if (!defined('IN_PHPBB'))
@@ -21,9 +25,9 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, [
 	'MEDIA_EMBED_PRIVACY_POLICY' => '
 		<br><br>
-		<h3>Embedded Content from Other Websites</h3>
-		“%1$s” may include posts or content that contain embedded material from external websites, including but not limited to YouTube, Facebook, Twitter, and similar platforms. Embedded content from these external sites behaves in the same way as if you had visited the originating website directly.
-		<br><br>These external websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with the embedded content, including tracking your interaction if you have an account and are logged in to that website.
-		<br><br>Please note that such activity is beyond the control of “%1$s” and is governed by the privacy policies and terms of service of the respective external websites. We encourage you to review the privacy and cookie policies of any third-party services you interact with through embedded content.
+		<h3>Contenu intégré d‘autres sites Web</h3>
+		“%1$s" peut inclure des publications ou du contenu qui contient du matériel intégré provenant de sites Web externes, y compris, mais sans s’y limiter, YouTube, Facebook, Twitter et des plateformes similaires. Le contenu intégré de ces sites externes se comporte de la même manière que si vous aviez visité directement le site d’origine.
+		<br><br>Ces sites Web externes peuvent collecter des données vous concernant, utiliser des cookies, intégrer un suivi supplémentaire de tiers et surveiller votre interaction avec le contenu intégré, y compris suivre votre interaction si vous avez un compte et êtes connecté à ce site Web.
+		<br><br>Veuillez noter que cette activité est indépendante de la volonté de "%1$s" et qu’elle est régie par les politiques de confidentialité et les conditions d’utilisation des sites Web externes respectifs. Nous vous encourageons à consulter les politiques de confidentialité et de cookies de tout service tiers avec lequel vous interagissez via du contenu intégré.
 	',
 ]);
